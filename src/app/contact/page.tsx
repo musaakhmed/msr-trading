8b16b190cc58399c120dac82594f4b22cc2da6d2
Musa Akhmedov <musa.punhan@pm.me>
2023-12-24 19:28:35 +0100

    feat: add contact form page and component with content and animation

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const contact = () => {
 	return (
-		<main className="flex h-screen items-center justify-center text-xl">
+		<main className="mt-16 flex min-h-screen flex-col items-center text-xl">
 			<Contact />
 		</main>
 	);
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,119 @@
-import React from 'react';
+'use client';
+
+import { motion } from 'framer-motion';
+
+const formVars = {
+	initial: {
+		opacity: 0,
+		x: -100,
+		transition: {
+			duration: 0.5,
+			ease: [0.1, 0.03, 0.5, 0.9],
+		},
+	},
+
+	animate: {
+		opacity: 1,
+		x: 0,
+		transition: {
+			duration: 0.5,
+			ease: [0.1, 0.03, 0.5, 0.9],
+		},
+	},
+};
+
+const containerVars = {
+	initial: {
+		transition: { staggerChildren: 0.5 },
+	},
+	animate: {
+		transition: { staggerChildren: 0.9 },
+	},
+};
 
 export const Contact = () => {
-	return <div>Contact</div>;
+	return (
+		<motion.div
+			variants={containerVars}
+			initial="initial"
+			animate="animate"
+			className="h-screen w-full bg-primary px-5 py-6 text-[1rem]"
+		>
+			<motion.section
+				variants={formVars}
+				initial="initial"
+				animate="animate"
+				className="text-background"
+			>
+				<h1 className="text-3xl font-bold ">Contact Us</h1>
+				<div className="pt-2">
+					<p>Have questions or inquiries? Reach out to us!</p>
+					<p className="my-2 ">
+						Your path to cutting-edge dental solutions begins with a
+						simple message.
+					</p>
+				</div>
+			</motion.section>
+
+			<motion.form
+				variants={formVars}
+				initial="initial"
+				animate={{
+					opacity: 1,
+					x: 0,
+					transition: {
+						duration: 0.5,
+						ease: [0.1, 0.03, 0.5, 0.9],
+						delay: 0.25,
+					},
+				}}
+				className=" rounded-xl bg-background p-4 "
+			>
+				<div className="my-5 flex flex-col gap-0.5 first:mt-0 ">
+					<label className="text" htmlFor="firstname">
+						First name
+					</label>
+					<input
+						className="focus:border-1 h-10 rounded border border-solid border-slate-300 p-2 focus:border-primary focus:shadow focus:outline-none"
+						type="text"
+						placeholder=""
+					></input>
+				</div>
+				<div className="my-5 flex flex-col gap-1">
+					<label className="text" htmlFor="lastname">
+						Last name
+					</label>
+					<input
+						className="focus:border-1 h-10 rounded border border-solid border-slate-300 p-2 focus:border-primary focus:shadow focus:outline-none"
+						type="text"
+						placeholder=""
+					></input>
+				</div>
+				<div className="my-5 flex flex-col gap-1">
+					<label className="text" htmlFor="">
+						email
+					</label>
+					<input
+						className="focus:border-1 h-10 rounded border border-solid border-slate-300 p-2 focus:border-primary focus:shadow focus:outline-none"
+						type="email"
+					></input>
+				</div>
+				<div className="my-5 flex flex-col gap-1">
+					<label className="text" htmlFor="message">
+						Message
+					</label>
+					<textarea
+						rows={6}
+						placeholder="Type your message"
+						className="focus:border-1 resize-none rounded border border-solid border-slate-300 p-2 focus:border-primary focus:shadow focus:outline-none"
+					></textarea>
+				</div>
+				<div className="flex ">
+					<button className=" w-full rounded-lg bg-primary p-2 text-secondary ">
+						Submit
+					</button>
+				</div>
+			</motion.form>
+		</motion.div>
+	);
 };
