4e2d34faff76407f10a413a9b3c992eaced04bdb
Musa Akhmedov <musa.punhan@pm.me>
2024-03-02 16:25:11 +0100

    chore: restructure all home page components into a HomePage folder for better clasity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import Industries from '@/components/Industries/Industries';
-import Hero from '@/components/ui/Hero/Hero';
-import Faq from '@/components/FAQ/FAQ';
+import Industries from '@/components/HomePage/Industries/Industries';
+import Hero from '@/components/HomePage/Hero/Hero';
+import Faq from '@/components/HomePage/Faq/Faq';
 
 export default function Home() {
 	return (
