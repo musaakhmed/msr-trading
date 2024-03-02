056a6ed93e1acb18abe34ebdf403d7f4f95389e9
Musa Akhmedov <musa.punhan@pm.me>
2024-03-02 16:23:56 +0100

    style: finalise the FAQ section content

--- src/components/HomePage/Faq/Faq.tsx
+++ src/components/HomePage/Faq/Faq.tsx
@@ -6,27 +6,27 @@ import {
 	AccordionTrigger,
 } from '@/components/ui/accordion';
 import { motion } from 'framer-motion';
-import { faq } from '@/data/faq';
+import { faqs } from '@/data/faqs';
 
 const Faq = () => {
 	return (
 		<motion.div
-			className="mx-auto flex min-h-screen flex-col items-center justify-center p-8"
+			className="mx-auto flex min-h-screen flex-col items-center justify-start p-8"
 			initial={{ y: 25, opacity: 0 }}
 			whileInView={{
 				y: 0,
 				opacity: 1,
 				transition: {
-					duration: 1,
+					duration: 0.75,
 					ease: [0.1, 0.03, 0.5, 0.9],
-					delay: 1,
+					delay: 0.5,
 				},
 			}}
 		>
-			{faq.map(({ question, answer }, i) => (
+			{faqs.map(({ question, answer }, i) => (
 				<Accordion key={i} type="single" collapsible className="w-full">
 					<AccordionItem value={i.toString()}>
-						<AccordionTrigger className="font-bold hover:no-underline">
+						<AccordionTrigger className="font-bold">
 							{question}
 						</AccordionTrigger>
 						<AccordionContent>{answer}</AccordionContent>
---
+++ src/data/faqs.js
@@ -0,0 +1,30 @@
+export const faqs = [
+	{
+		question: 'What is the mission of MSR Trading?',
+		answer: 'Our mission is to be your trusted partner on the Modern Silk Road, facilitating smooth equipment procurement across diverse industries and borders. We connect companies in Europe, Russia, and Azerbaijan, fostering collaboration and economic growth.',
+	},
+	{
+		question: 'What are your core values?',
+		answer: 'We value collaboration, transparency, and impact. We build strong partnerships, communicate openly, and strive to empower businesses and contribute to positive change in the regions we serve.',
+	},
+	{
+		question: 'Where are you located?',
+		answer: 'We have offices in Brussels, Belgium and Istanbul, Turkey. This strategic positioning allows us to serve clients across Europe, Asia, and Africa efficiently.',
+	},
+	{
+		question: 'What languages do you speak?',
+		answer: 'Our multilingual team speaks English, Russian, Turkish, French, and more. This allows us to communicate effectively with clients from diverse backgrounds and navigate cultural nuances.',
+	},
+	{
+		question: 'Do you offer additional services beyond equipment sales?',
+		answer: 'Yes, we offer expert consultation services to help you source the right equipment for your specific needs and navigate the procurement process.',
+	},
+	{
+		question: 'Do you ship internationally?',
+		answer: 'Yes, we have a strong logistics network and ship equipment anywhere in Europe, Africa and Asia.',
+	},
+	{
+		question: 'How can I request a quote?',
+		answer: 'You can easily request a quote through our website contact form or by directly reaching out to our sales team.',
+	},
+];
