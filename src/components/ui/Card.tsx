50ce79b2e80e882e0379a953e0f0298d2a611928
Musa Akhmedov <musa.punhan@pm.me>
2024-03-10 13:47:54 +0100

    fix: set better image width and card layout for smaller desktop screens

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -37,16 +37,16 @@ const Card = ({
 			className="sticky top-0 mx-4 flex h-screen items-center justify-center pt-24"
 		>
 			<motion.section
-				className="relative flex h-4/5 w-full flex-col gap-2 rounded-xl bg-primary text-primary-foreground sm:w-4/5 lg:mx-auto lg:flex-row-reverse lg:items-center lg:justify-center "
+				className="relative flex h-4/5 w-full flex-col gap-2 rounded-xl bg-primary text-primary-foreground sm:w-4/5 xl:mx-auto xl:flex-row-reverse xl:items-center xl:justify-center "
 				style={{ scale, top: `calc(-10% + ${i * 25}px)` }}
 			>
-				<div className="lg: h-1/3 w-full overflow-hidden rounded-t-xl lg:h-1/2 lg:rounded-none ">
+				<div className="w-full overflow-hidden rounded-t-xl lg:h-1/3 xl:h-1/2 xl:rounded-none ">
 					<motion.div
-						className="h-full w-full"
+						className="h-full w-full xl:p-8"
 						style={{ scale: imageScale }}
 					>
 						<Image
-							className="object-center"
+							className="object-center "
 							width={800}
 							height={780}
 							src={img}
@@ -54,8 +54,8 @@ const Card = ({
 						/>
 					</motion.div>
 				</div>
-				<div className="mx-4 flex flex-col justify-around gap-8 py-4 lg:w-3/4 lg:p-8">
-					<div className="text-2xl font-black lg:text-5xl">
+				<div className="mx-4 flex flex-col justify-around gap-8 py-4 lg:p-8 xl:w-3/4">
+					<div className=" text-2xl font-black lg:text-4xl xl:text-5xl ">
 						{title}
 					</div>
 					<p className="text-lg lg:text-xl xl:text-2xl">{desc}</p>
