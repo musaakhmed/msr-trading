ac361866be2782b53274fc1143bec3c39bbd44ae
Musa Akhmedov <musa.punhan@pm.me>
2024-03-10 19:33:24 +0100

    chore: trials on the brands section

--- src/components/HomePage/Brands/Brands.tsx
+++ src/components/HomePage/Brands/Brands.tsx
@@ -1,10 +1,13 @@
 import { ImageBox } from '@/components/ui/ImageBox';
+import { brands } from '@/data/brands';
 
 export const Brands = () => {
 	return (
 		<section className="flex h-screen w-full items-center justify-center bg-accent">
 			<h2>Medical and Lab brands</h2>
-			<ImageBox></ImageBox>
+			{brands.map(({ name, img, industry }, index) => (
+				<ImageBox key={index}></ImageBox>
+			))}
 		</section>
 	);
 };
