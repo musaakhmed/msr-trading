6118e8f73ce84dc3da1cbe370a6adb8687713ff2
Musa Akhmedov <musa.punhan@pm.me>
2024-03-16 15:33:40 +0100

    chore: add fake brands data

--- src/data/brands.js
+++ src/data/brands.js
@@ -4,4 +4,84 @@ export const brands = [
 		name: 'Abbott',
 		img: '/assets/images/brands/medical/abbott.png',
 	},
+	{
+		industry: 'medical',
+		name: 'B Braun',
+		img: '/assets/images/brands/medical/b_braun.png',
+	},
+	{
+		industry: 'medical',
+		name: 'Bard',
+		img: '/assets/images/brands/medical/bard.png',
+	},
+	{
+		industry: 'medical',
+		name: 'BD',
+		img: '/assets/images/brands/medical/bd.png',
+	},
+	{
+		industry: 'medical',
+		name: 'Boston Scientific',
+		img: '/assets/images/brands/medical/boston_scientific.png',
+	},
+	{
+		industry: 'medical',
+		name: 'Convatec',
+		img: '/assets/images/brands/medical/convatec.png',
+	},
+	{
+		industry: 'medical',
+		name: 'Covidien',
+		img: '/assets/images/brands/medical/covidien.png',
+	},
+	{
+		industry: 'medical',
+		name: 'Ethicon',
+		img: '/assets/images/brands/medical/ethicon.png',
+	},
+	{
+		industry: 'medical',
+		name: 'GE Healthcare',
+		img: '/assets/images/brands/medical/ge_healthcare.png',
+	},
+	{
+		industry: 'medical',
+		name: 'Hartmann AG',
+		img: '/assets/images/brands/medical/hartmann_ag.png',
+	},
+	{
+		industry: 'medical',
+		name: 'Karl Storz',
+		img: '/assets/images/brands/medical/karl_storz.png',
+	},
+	{
+		industry: 'medical',
+		name: 'Medtronic',
+		img: '/assets/images/brands/medical/medtronic.png',
+	},
+	{
+		industry: 'medical',
+		name: 'Nikon Kohden',
+		img: '/assets/images/brands/medical/nikon_kohden.png',
+	},
+	{
+		industry: 'medical',
+		name: 'Philips',
+		img: '/assets/images/brands/medical/philips.png',
+	},
+	{
+		industry: 'medical',
+		name: 'Smith & Nephew',
+		img: '/assets/images/brands/medical/smith_nephew.png',
+	},
+	{
+		industry: 'medical',
+		name: 'Welch Allyn',
+		img: '/assets/images/brands/medical/welch-allyn.png',
+	},
+	{
+		industry: 'medical',
+		name: 'Zeiss',
+		img: '/assets/images/brands/medical/zeiss.png',
+	},
 ];
