65b0538a087ad657e2f3dfa3b585e9a289d35556
Musa Akhmedov <musa.punhan@pm.me>
2024-04-02 22:12:22 +0200

    style: brands section subtitle colour change and bg gradient

--- src/components/HomePage/Brands/Brands.tsx
+++ src/components/HomePage/Brands/Brands.tsx
@@ -3,13 +3,13 @@ import { medicalBrands, labBrands } from '@/data/brands';
 
 export const Brands = () => {
 	return (
-		<section className=" py-36">
+		<section className=" bg-gradient-to-b from-white via-accent/10 to-white py-36">
 			<div className="container flex h-full w-full flex-col items-center justify-center space-y-10">
-				<h2 className="w-3/4 bg-accent py-2 text-center text-xl font-bold text-foreground text-white md:text-2xl lg:text-4xl">
+				<h2 className="w-3/4 py-2 text-center text-xl font-bold text-accent md:text-2xl lg:text-4xl">
 					Our Top Brands
 				</h2>
 				<div className="space-y-4">
-					<h4 className="text-center text-lg font-bold md:text-xl lg:text-3xl">
+					<h4 className="text-center text-lg font-bold text-primary md:text-xl lg:text-3xl">
 						Medical Equipment and Supplies
 					</h4>
 					<div className="flex w-full flex-wrap items-center justify-center gap-8">
@@ -25,7 +25,7 @@ export const Brands = () => {
 					</div>
 				</div>
 				<div className="space-y-4">
-					<h4 className="text-center text-lg font-bold md:text-xl lg:text-3xl">
+					<h4 className="text-center text-lg font-bold text-primary md:text-xl lg:text-3xl">
 						Lab Equipment and Supplies
 					</h4>
 					<div className="flex w-full flex-wrap items-center justify-center gap-10">
